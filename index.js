b6f32148ed1751decd820f8b18c08d3adf34c9ad
Tran Thanh Duc <thanhduckrb767@gmail.com>
2022-07-13 10:37:33 +0700

    index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+var number1 = 1;
+console.log(number1++); // 1
+console.log(number1); // 2
+
+console.log(number1++); // 2
+console.log(number1); // 3
+
+console.log('--- Tiền tố ---');
+
+var number2 = 1;
+
+console.log(++number2); // 2
+console.log(number2); // 2
+
+console.log(++number2); // 3
+console.log(number2); // 3
